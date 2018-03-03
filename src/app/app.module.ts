2b2afeeda8b92e498d580f6c0d3fd29bf1e6e3ad
JuanQuerubin <Querubin.perez@udea.edu.co>
2018-03-03 08:30:40 -0500

    Actividad Adicional. Añadiendo un nuevo componente.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MaterialComponent } from './material/material.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    MaterialComponent
   ],
   imports: [
     BrowserModule,
